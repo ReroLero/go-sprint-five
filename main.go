998343fc9f269b7eec1ab91786a84519b3d0b623
Ziubina Valeria <drerolero@gmail.com>
2025-02-05 18:22:10 +0500

    3it

--- main.go
+++ main.go
@@ -81,14 +81,8 @@ func (i InfoMessage) String() string {
 // CaloriesCalculator интерфейс для структур: Running, Walking и Swimming.
 type CaloriesCalculator interface {
 	// добавьте необходимые методы в интерфейс
-	func (t Training) Calories() float64{
-		// вставьте ваш код ниже
-		return Calories()
-	}
-
-	func (t Training) TrainingInfo() InfoMessage {
-		// вставьте ваш код ниже
-		return TrainingInfo()
+	Calories()
+	TrainingInfo()
 	}
 }
 
